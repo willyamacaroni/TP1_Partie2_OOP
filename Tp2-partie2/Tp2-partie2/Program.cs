be555331599e1ca07bd4dba0c7a8cf2ddf26e5ea
willyam arcand <willyam.arcand@gmail.com>
2020-02-18 09:40:50 -0500

    First commit & push. There is currently nothing in this project.

---
+++ Tp2-partie2/Tp2-partie2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp2_partie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
